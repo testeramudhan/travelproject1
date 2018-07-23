63980885dc15e617562a26774cb9004cd012ca2f
testeramudhan <amudhan.good@gmail.com>
2018-07-23 10:27:31 +0530

    framework changed

---
+++ src/org/com/Hexaware.java
@@ -0,0 +1,17 @@
+package org.com;
+
+public class Hexaware {
+public void companyname() {
+System.out.println("Hexaware");
+}
+public void companyaddress() {
+	System.out.println("Sipcot");
+}
+public static void main(String[] args) {
+	Hexaware h = new Hexaware();
+	h.companyname();
+	h.companyaddress();
+	Techm t = new Techm();
+	t.companyDetails();
+}
+}
---
+++ src/org/com/Techm.java
@@ -0,0 +1,8 @@
+package org.com;
+
+public class Techm {
+	public void companyDetails() {
+System.out.println("TechMahindra");
+	}
+
+}
